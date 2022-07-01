7ffd99505745b50447146c4c7de0a7cbec18bc6f
waiphy0aung <bgyi354@gmail.com>
2022-07-01 17:36:25 +0630

    novel category chapter api

--- README.md
+++ README.md
@@ -17,3 +17,6 @@
 -   register_route - /api/auth/register
 -   email_verify route - /api/auth/email-verify
 -   token_scope route - /api/auth/login
+-   Novels route - /api/novels
+-   Chapters route - /api/chapters
+-   Categories route - /api/categories
--- app/Http/Controllers/API/ChapterController.php
+++ app/Http/Controllers/API/ChapterController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Chapter;
 use Illuminate\Http\Request;
 
 class ChapterController extends Controller
 {
-    //
+    public function index(){
+        return Chapter::with('novel')->get();
+    }
 }
